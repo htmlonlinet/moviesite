90fe1cc82244065183900f4ac429383755322028
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-16 20:32:26 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
 function search(){
   window.location.assign("search.html?movie=" + document.forms.search.movie.value);
+  document.getElementById("link").innerText = document.forms.search.movie.value;
+  document.getElementById("link").href = document.forms.search.movie.value;
 }
