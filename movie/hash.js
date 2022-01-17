6f6b2277d9044ea46925464d80320b6082968e44
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-17 13:47:49 -0500

    Update hash.js

--- movie/hash.js
+++ movie/hash.js
@@ -1,5 +1,5 @@
 if(window.location.href == "https://moviesto.netlify.app/movie/" + window.location.hash){
   var hash = window.location.hash;
   hash.split("#s=");
-  window.location.pathname = hash;
+  window.location.assign(hash + ".html");
 }
