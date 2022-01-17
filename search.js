49df37cea5676fbd2a233ff6c55e7d5cc266c47d
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-16 20:41:21 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,7 +1,3 @@
 function search(){
   window.location.assign("search.html?movie=" + document.forms.search.movie.value);
-  var link = document.createElement("link");
-  link.innerText = window.location.search;
-  link.href = "javascript:" + window.location.search;
-  document.body.appendChild(link);
 }
